fb8fba3fcb63157266e219872bfc0b0cb37ef51c
Koha Choji <koha.choji@vistair.com>
2020-06-09 18:59:17 +0100

    Refactor duplicate DB code into separate function

--- src/server.js
+++ src/server.js
@@ -5,37 +5,36 @@ import { MongoClient } from 'mongodb';
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/api/articles/:name', async (req, res) => {
+const withDb = async (operations, res) => {
     try {
-        const articleName = req.params.name;
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
         const db = client.db('myBlogDb');
 
-        const articleInfo = await db.collection('articles').findOne({ name: articleName });
-        res.status(200).json(articleInfo);
-        
-        client.close();
-    } catch (err) {
+        await operations(db);
+
+        client.close();;
+
+    } catch(error) {
         res.status(500).json({ message: 'Error connecting to DB', error });
     }
+};
+
+app.get('/api/articles/:name', async (req, res) => {
+    withDb(async (db) => {
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+    }, res);
 });
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
-    try {
+    withDb(async (db) => {
         const articleName = req.params.name;
-
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-        const db = client.db('myBlogDb');
-
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
         await db.collection('articles').updateOne({name: articleName}, { $set: { upvotes: articleInfo.upvotes + 1 }});
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
-
         res.status(200).json(updatedArticleInfo);
-        client.close();
-    } catch (err) {
-        res.status(500).json({ message: 'Error connecting to DB', error });
-    }
+    }, res);
 });
 
 app.post('/api/articles/:name/add-comment', (req, res)=> {
