a7cd89c1c12e51a5f49f8b01332d5277a3eda555
Koha Choji <koha.choji@vistair.com>
2020-06-09 14:28:10 +0100

    Implemented a static upvote article

--- src/server.js
+++ src/server.js
@@ -1,7 +1,24 @@
 import express from 'express';
 
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0
+    },
+    'learn-rails': {
+        upvotes: 0
+    },
+    'learn-spring': {
+        upvotes: 0
+    }
+};
+
 const app = express();
 
-app.get('/hello', (req, res) => res.send('Hello!'));
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes.`);
+});
 
 app.listen(8000, () => console.log('Listening on port 8000'));
