f9cd5b4c42f351cbaa558252d2669db61865b07b
Koha Choji <koha.choji@vistair.com>
2020-06-12 11:11:26 +0100

    Copied packaged react app to serve

---
+++ src/build/precache-manifest.4e5184061529d1222ac8480dd6b2ef0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd18b42f15b7c23291f76084ad71225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2858b5a4d7a50549460",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "894f3e8d22e5bdc828d9",
+    "url": "/static/js/2.8ed1261c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8ed1261c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2858b5a4d7a50549460",
+    "url": "/static/js/main.e9725a34.chunk.js"
+  },
+  {
+    "revision": "b5ff7b7d731d2afb06b6",
+    "url": "/static/js/runtime-main.605a4372.js"
+  }
+]);
