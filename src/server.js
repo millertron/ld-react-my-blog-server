b12b05d0dcd2957bda3a1619053c0b161bac37cb
Koha Choji <koha.choji@vistair.com>
2020-06-12 11:17:11 +0100

    Serve packaged react app

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 import { MongoClient } from 'mongodb';
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(bodyParser.json());
 
 const withDb = async (operations, res) => {
@@ -47,6 +50,10 @@ app.post('/api/articles/:name/add-comment', async (req, res)=> {
         const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
         res.status(200).json(updatedArticleInfo);
     }, res);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
